f582c54b718895f2f2d64ed07e8fdb3030346ba5
UlfetZakirli <ulfetzakirli@gmail.com>
2022-10-10 19:52:13 +0400

    senua-feat: first trial of FRIENDS page

---
+++ client/src/components/pages/single-user/SingleUser.jsx
@@ -0,0 +1,153 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function SingleUser() {
+  return (
+    <List sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Ulfat Zakirli"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                @Zakirli
+              </Typography>
+              <Typography
+                sx={{}}
+                component="span"
+                variant="body2"
+                color="text.primary"
+                style={{display:'flex',justifyContent:'flex-end'}}
+            
+              >
+              <Button size="small" style={{marginTop:'-30px',fontSize:'13px',backgroundColor:'#000'}} variant="contained">Unfriend</Button>
+                
+              </Typography>
+            </React.Fragment>
+          }
+        />
+
+      </ListItem>
+      <Divider variant="inset" component="li"/>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Fatima Mirzezade"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                @F_Mirzezade
+              </Typography>
+              <Typography
+                sx={{}}
+                component="span"
+                variant="body2"
+                color="text.primary"
+                style={{display:'flex',justifyContent:'flex-end'}}
+            
+              >
+              <Button size="small" style={{marginTop:'-30px',fontSize:'13px',backgroundColor:'#000'}} variant="contained">Unfriend</Button>
+                
+              </Typography>
+            </React.Fragment>
+          }
+        />
+
+      </ListItem>
+      <Divider variant="inset" component="li"/>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Kamran Ekhberov"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                @Ekhberov_22
+              </Typography>
+              <Typography
+                sx={{}}
+                component="span"
+                variant="body2"
+                color="text.primary"
+                style={{display:'flex',justifyContent:'flex-end'}}
+            
+              >
+              <Button size="small" style={{marginTop:'-30px',fontSize:'13px',backgroundColor:'#000'}} variant="contained">Unfriend</Button>
+                
+              </Typography>
+            </React.Fragment>
+          }
+        />
+
+      </ListItem>
+      <Divider variant="inset" component="li"/>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Efsane Sadiqova"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                @Efso23
+              </Typography>
+              <Typography
+                sx={{}}
+                component="span"
+                variant="body2"
+                color="text.primary"
+                style={{display:'flex',justifyContent:'flex-end'}}
+            
+              >
+              <Button size="small" style={{marginTop:'-30px',fontSize:'13px',backgroundColor:'#000'}} variant="contained">Unfriend</Button>
+                
+              </Typography>
+            </React.Fragment>
+          }
+        />
+
+      </ListItem>
+      <Divider variant="inset" component="li"/>
+
+
+
+
+  
+    </List>
+  );
+}
--- client/src/pages/friends/index.jsx
+++ client/src/pages/friends/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import SingleUser from "components/pages/single-user/SingleUser";
 
 const FriendsPage = () => {
-  return <div>FriendsPage</div>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" style={{height:'90vh',backgroundColor:'#eff3f4',marginTop:'20px'}}>
+        <Stack style={{marginTop:'40px',marginLeft:'20px'}} spacing={2} direction="row">
+      <Button style={{width:'250px'}} size="large" variant="outlined">Add Friends</Button>
+      <Button style={{width:'250px'}} size="large" variant="contained">Requests</Button>
+    </Stack>
+    <Container maxWidth="sm" style={{height:'70vh',backgroundColor:'#fff',marginTop:'20px'}}>
+<SingleUser/>
+    </Container>
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default FriendsPage;
