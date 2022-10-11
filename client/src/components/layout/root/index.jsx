5ff2a1ea882978746bc8c5e2727b3af6f5da61e9
suleyman1406 <dadasovsuleyman126@gmail.com>
2022-10-11 23:45:51 +0400

    senua-feat: AuthGuard added to client side and fix on auth management

--- client/src/components/layout/root/index.jsx
+++ client/src/components/layout/root/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import AuthGuard from "../auth-guard";
 import Sidebar from "../sidebar";
 
 const Root = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <Sidebar />
-      <div style={{ width: "calc(100% - 270px)", marginLeft: "auto" }}>
-        <Outlet />
+    <AuthGuard>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ width: "calc(100% - 270px)", marginLeft: "auto" }}>
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </AuthGuard>
   );
 };
 
