8f9127436b4bce34348a4a3311b7e52edccf471d
nargiz1 <nargizer@code.edu.az>
2022-10-13 00:52:24 +0400

    senua-nargiz: postCreate updated

---
+++ client/src/components/layout/post/postStyle.js
@@ -0,0 +1,40 @@
+export const styles = {
+  module: {
+    position: "fixed",
+    top: "20px",
+    left: "50%",
+    transform: "translateX(-50%)",
+    backgroundColor: "white",
+    padding: "10px 0px",
+    boxShadow: "0 4px 8px 0 rgb(0 0 0 / 10%)",
+    width: "400px",
+    borderRadius: "10px",
+    zIndex: "10",
+  },
+  line: {
+    height: "1px",
+    backgroundColor: "rgb(0 0 0 / 10%)",
+  },
+  iconWrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "6px 7px 6px 20px",
+  },
+  text: {
+    border: "none",
+    resize: "none",
+    padding: "20px",
+    width: "100%",
+    height: "100px",
+  },
+  wrapper: {
+    padding: "10px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  imageUrl: {
+    fontSize: "12px",
+    marginLeft: "5px",
+  },
+};
---
+++ client/src/components/layout/post/utils.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+export function checkIfFilesAreCorrectType(file) {
+  if (file) {
+    if (!SUPPORTED_FORMATS.includes(file.type)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function checkIfFilesAreCorrectSize(file) {
+  if (file) {
+    const size = file.size / 1024 / 1024;
+    if (size > 10) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const currentUser = JSON.parse(localStorage.getItem("user"));
+
+export const createPost = async (data) => {
+    console.log(data);
+    const { data: response } = await axios.post(
+      "http://localhost:8080/api/posts/create",
+      data,
+      {
+        headers: {
+          "x-access-token": currentUser?.token,
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "multipart/form-data",
+          Accept: "application/json",
+        },
+      }
+    );
+    return response;
+  };
